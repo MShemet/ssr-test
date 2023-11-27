681ce70b0f915ab7f467d506a34e4d39ea7b358d
Maksym Shemet <max.shemet@gmail.com>
2023-11-27 09:26:07 +0200

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,64 @@
+import { resolve } from 'path';
+import { defineConfig, type PluginOption } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import ViteSvgSpriteWrapper from 'vite-svg-sprite-wrapper';
+import postcssNested from 'postcss-nested';
+import stylelint from 'stylelint';
+
+export default defineConfig(({ command }) => {
+  const plugins: PluginOption[] = [
+    ViteSvgSpriteWrapper({
+      icons: 'src/svg-sprite/*.svg',
+      outputDir: 'public/images/',
+    }),
+  ];
+
+  if (command === 'build') {
+    plugins.push(
+      ViteImageOptimizer({
+        exclude: ['sprite.svg'],
+      }),
+    );
+  }
+
+  return {
+    plugins,
+
+    build: {
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          home: resolve(__dirname, 'src/pages/home/home.ts'),
+        },
+        output: {
+          entryFileNames: 'assets/[name].js',
+          chunkFileNames: 'assets/[name].js',
+          assetFileNames: 'assets/[name].[ext]',
+        },
+      },
+    },
+
+    css: {
+      postcss: {
+        plugins: [
+          postcssNested(),
+          stylelint({
+            config: {
+              extends: ['stylelint-config-standard'],
+            },
+          }),
+        ],
+      },
+    },
+
+    // resolve: {
+    //   alias: {
+    //     '@': new URL('./src', import.meta.url).pathname,
+    //   },
+    // },
+
+    // server: {
+    //   host: true,
+    // },
+  };
+});
